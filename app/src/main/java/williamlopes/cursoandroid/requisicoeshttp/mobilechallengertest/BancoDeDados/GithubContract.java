37253564b1e89ec0b21815c47d5967daf94e74bf
William Lopes <williamlopes64@gmail.com>
2020-04-21 20:00:27 -0300

    adicionando a tabela owner

--- app/src/main/java/williamlopes/cursoandroid/requisicoeshttp/mobilechallengertest/BancoDeDados/GithubContract.java
+++ app/src/main/java/williamlopes/cursoandroid/requisicoeshttp/mobilechallengertest/BancoDeDados/GithubContract.java
@@ -11,9 +11,10 @@ public class GithubContract {
         public static final String colunaId = "id";
     }
 
-    /*public static final class ItemsEntry implements BaseColumns {
-        public static final String tabelaItem = "item";
-        public static final String colunaName = "name";
-        public static final String colunaDescription = "description";
-    }*/
+    public static final class OwnerEntry implements BaseColumns {
+        public static final String tabelaNome = "owner";
+        public static final String colunaLogin = "login";
+        public static final String colunaAvatar = "avatar";
+        public static final String colunaId = "id";
+    }
 }
