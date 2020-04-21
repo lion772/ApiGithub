8ed5f29826674a5c835e7423d7d3179fc84e0906
William Lopes <williamlopes64@gmail.com>
2020-04-21 19:59:32 -0300

    adicionando id e avatar

--- app/src/main/java/williamlopes/cursoandroid/requisicoeshttp/mobilechallengertest/model/DadosUsuario.java
+++ app/src/main/java/williamlopes/cursoandroid/requisicoeshttp/mobilechallengertest/model/DadosUsuario.java
@@ -4,6 +4,19 @@ public class DadosUsuario {
 
     public String login;
     public String avatar_url;
+    public Integer id;
+
+    public void setAvatar_url(String avatar_url) {
+        this.avatar_url = avatar_url;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getLogin() {
         return login;
