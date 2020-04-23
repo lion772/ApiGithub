22b5ac2a277ef599b6807c7c86026348d4ef02d1
William Lopes <williamlopes64@gmail.com>
2020-04-23 18:54:21 -0300

    Adicionando a coluna full_name no SQLite

--- app/src/main/java/williamlopes/cursoandroid/requisicoeshttp/mobilechallengertest/BancoDeDados/SQLite.java
+++ app/src/main/java/williamlopes/cursoandroid/requisicoeshttp/mobilechallengertest/BancoDeDados/SQLite.java
@@ -10,7 +10,7 @@ public class SQLite extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "github.db"; //nome
 
     // If you change the database schema, you must increment the database version
-    private static final int DATABASE_VERSION = 3; //quando alterar o onCreate, altere a versão
+    private static final int DATABASE_VERSION = 4; //quando alterar o onCreate, altere a versão
 
     // Constructor
     public SQLite(Context context) {
@@ -25,7 +25,8 @@ public class SQLite extends SQLiteOpenHelper {
                 GithubContract.ItemsEntry.colunaId + " INTEGER PRIMARY KEY," +
                 GithubContract.ItemsEntry.colunaName + " TEXT NOT NULL, " +
                 GithubContract.ItemsEntry.colunaDescription + " TEXT ," +
-                GithubContract.ItemsEntry.colunaIdOwner + " INTEGER " +
+                GithubContract.ItemsEntry.colunaIdOwner + " INTEGER, " +
+                GithubContract.ItemsEntry.colunaFullName + " TEXT " +
                 "); ";
 
         db.execSQL(SQL_CREATE_ITEMS_TABLE);
