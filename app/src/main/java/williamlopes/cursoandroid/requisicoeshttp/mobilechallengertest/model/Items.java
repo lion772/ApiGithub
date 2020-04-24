ca1ea279614a40a7fa83479c050776b93ec95476
William Lopes <williamlopes64@gmail.com>
2020-04-24 20:35:25 -0300

    Adicionando outros atributos à classe Items: language, open_issues e created_at

--- app/src/main/java/williamlopes/cursoandroid/requisicoeshttp/mobilechallengertest/model/Items.java
+++ app/src/main/java/williamlopes/cursoandroid/requisicoeshttp/mobilechallengertest/model/Items.java
@@ -8,19 +8,45 @@ import java.util.List;
 public class Items implements Serializable {
     @SerializedName("owner")
     public DadosUsuario owner;
-
     @SerializedName("name")
     public String name;
-
     @SerializedName("description")
     public String description;
-
     @SerializedName("id")
     public Integer id;
+    @SerializedName("language")
+    public String language;
+    @SerializedName("open_issues")
+    public String open_issues;
+    @SerializedName("created_at")
+    public String created_at;
 
     public Integer idOwner; //Não tem valor referente na API para idOwner, porém é importante esse atributo para usar como parâmetro de comparação a fim de filtrar futuramente no banco de dados, e identificar cada usuário e seus respectivos repositórios
 
 
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getOpen_issues() {
+        return open_issues;
+    }
+
+    public void setOpen_issues(String open_issues) {
+        this.open_issues = open_issues;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
 
     public Integer getIdOwner() {
         return idOwner;
